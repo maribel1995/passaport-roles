e277b1c2928c7c0965771bfec391e69614e3b792
Mariana <marianamrs1995@hotmail.com>
2019-02-05 22:13:52 -0200

    users adicionado

--- models/users.js
+++ models/users.js
@@ -1,2 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const usersSchema = new Schema ({
+    name:String,
+    bio: String,
+    courses: [{type: Schema.Types.ObjectId, ref:'Courses'}],
+    role: {
+        type: String,
+        enum:['BOSS', 'DEVELOPER', 'TA', 'STUDENT']
+    }
+},{
+    timestamps: {
+        createdAt: "created_at",
+        updatedAt: "updated_at"
+    }
+});
+
+const Users = mongoose.model('Users', usersSchema);
+module.exports = Users;
